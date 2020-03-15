61443cb222a4322a45d960f00741a9b28364769d
Pavel Ivashkov <pavels.code@gmail.com>
2020-03-15 03:22:27 +0200

    add ios example

---
+++ Example/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+import PcgRandom
+import UIKit
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var dice1: Dice!
+    @IBOutlet weak var dice2: Dice!
+    
+    @IBAction func handleTap(_ sender: Any) {
+        rollDice()
+    }
+    
+    func rollDice() {
+        var pcg = Pcg64Random(seed: .random(in: 0...(.max)))
+        let roll = { (sides:Int) in Int.random(in: 1...sides, using: &pcg) }
+        
+        dice1.value = roll(6)
+        dice2.value = roll(6)
+    }
+}
