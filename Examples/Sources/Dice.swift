91dd7e8689e23fa45ed01825fc4c9875a8c049a4
Pavel Ivashkov <pavels.code@gmail.com>
2020-03-15 03:22:27 +0200

    add ios example

---
+++ Examples/Sources/Dice.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+
+class Dice : UIControl {
+    
+    weak var label: UILabel!
+    
+    var value: Int = 0 {
+        didSet {
+            label.text = "\(value)"
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        layer.borderColor = UIColor.darkGray.cgColor
+        layer.borderWidth = 1
+        
+        let label = UILabel()
+        label.frame = self.bounds
+        label.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        label.font = UIFont.boldSystemFont(ofSize: 100)
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .center
+        addSubview(label)
+        self.label = label
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        layer.cornerRadius = bounds.width / 10
+    }
+}
