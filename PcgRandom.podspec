49875f8560be4c4593f8a1f643d49ceffc9bf72c
Pavlo Ivashkov <pavels.code@gmail.com>
2025-05-06 21:46:21 +0300

    update to Swift 6

--- PcgRandom.podspec
+++ PcgRandom.podspec
@@ -6,16 +6,11 @@ Pod::Spec.new do |s|
   s.license          = 'MIT'
   s.author           = 'Pavel Ivashkov'
   s.source           = { :git => 'https://github.com/paiv/swift-pcg-random.git', :tag => s.version.to_s }
-  s.swift_version    = '5.0'
-  s.macos.deployment_target = '10.13'
-  s.ios.deployment_target = '12.0'
-  s.watchos.deployment_target = '9.0'
+  s.swift_version    = '6.1'
+  s.macos.deployment_target = '15.0'
+  s.ios.deployment_target = '18.0'
+  s.watchos.deployment_target = '11.0'
 
   s.source_files = 'Sources/PcgRandom/**/*.swift'
-
-  s.subspec 'PcgRandomC' do |cs|
-      cs.source_files = 'Sources/PcgRandomC/**/*.{h,hpp,c,cpp}'
-      cs.public_header_files = 'Sources/PcgRandomC/include/**/*.h'
-  end
 
 end
--- Sources/PcgRandom/PcgRandom.swift
+++ Sources/PcgRandom/PcgRandom.swift
@@ -1,32 +1,31 @@
+import Synchronization
 
-public class Pcg64Random : RandomNumberGenerator {
+
+public final class Pcg64Random : RandomNumberGenerator, Sendable {
     // pcg64 = pcg_engines::setseq_xsl_rr_128_64
 
-    public init(seed: UInt128, increment: UInt128, lock: PcgRandomLocking) {
-        self.lock = lock
-        self.state = Self.bump(state: increment &+ seed, increment: increment)
+    public init(seed: UInt128, increment: UInt128) {
+        self.state = .init(Self.bump(state: increment &+ seed, increment: increment))
         self.increment = increment
     }
 
-    private var state: UInt128
+    private let state: Mutex<UInt128>
     private let increment: UInt128
-    private var lock: PcgRandomLocking
 
-    private static let multiplier = UInt128(lo: 4865540595714422341, hi: 2549297995355413924)
-    private static let defaultIncrement = UInt128(lo: 1442695040888963407, hi: 6364136223846793005)
+    private static let multiplier = UInt128(0x2360ed051fc65da44385df649fccf645)
+    private static let defaultIncrement = UInt128(0x5851f42d4c957f2d14057b7ef767814f)
 
     public func next() -> UInt64 {
-        lock.lock()
-        defer { lock.unlock() }
+        state.withLock { store in
+            let state = Self.bump(state: store, increment: increment)
 
-        let state = Self.bump(state: self.state, increment: increment)
+            let rot = UInt32(truncatingIfNeeded: (state >> 122))
+            let xored = UInt64(truncatingIfNeeded: (state >> 64) ^ state)
+            let result = (xored >> rot) | (xored << ((-Int32(truncatingIfNeeded: rot)) & 63))
 
-        let rot = UInt32(truncatingIfNeeded: (state >> 122).lo)
-        let xored = state.hi ^ state.lo
-        let result = (xored >> rot) | (xored << ((-Int32(truncatingIfNeeded: rot)) & 63))
-
-        self.state = state
-        return result
+            store = state
+            return result
+        }
     }
 
     @inline(__always)
@@ -34,62 +33,37 @@ public class Pcg64Random : RandomNumberGenerator {
         return state &* Self.multiplier &+ increment
     }
 
-    class func defaultLock() -> PcgRandomLocking {
-        #if os(Linux)
-            return PThreadMutex()
-        #else
-
-        if #available(macOS 10.12, iOS 10.0, watchOS 3.0, tvOS 10.0, *) {
-            return OSUnfairLock()
-        }
-        else {
-            return PThreadMutex()
-        }
-
-        #endif
-    }
-
     public func advance(by delta: UInt128) {
-        lock.lock()
-        defer { lock.unlock() }
-
         var delta = delta
-        let zero = UInt128(0)
-        let one = UInt32(1)
         var cur_mult = Self.multiplier
         var cur_plus = increment
         var acc_mult = UInt128(1)
         var acc_plus = UInt128(0)
-        while (zero < delta) {
-           if (delta & one != 0) {
+        while (delta > 0) {
+           if (delta & 1 != 0) {
               acc_mult &*= cur_mult
               acc_plus = acc_plus &* cur_mult &+ cur_plus
            }
-           cur_plus = (cur_mult &+ one) &* cur_plus
+           cur_plus = (cur_mult &+ 1) &* cur_plus
            cur_mult &*= cur_mult
            delta >>= 1;
         }
-        state = acc_mult &* state &+ acc_plus
+
+        state.withLock { store in
+            store = acc_mult &* store &+ acc_plus
+        }
     }
 }
 
 
 public extension Pcg64Random {
 
-    convenience init(seed: UInt128, stream: UInt128, lock: PcgRandomLocking) {
-        self.init(seed: seed, increment: ((stream << 1) | 1), lock: lock)
-    }
-
-    convenience init(seed: UInt128, lock: PcgRandomLocking) {
-        self.init(seed: seed, increment: Self.defaultIncrement, lock: lock)
-    }
-
     convenience init(seed: UInt128, stream: UInt128) {
-        self.init(seed: seed, stream: stream, lock: Self.defaultLock())
+        self.init(seed: seed, increment: ((stream << 1) | 1))
     }
 
     convenience init(seed: UInt128) {
-        self.init(seed: seed, lock: Self.defaultLock())
+        self.init(seed: seed, increment: Self.defaultIncrement)
     }
 
     func discard(_ delta: UInt128) {
@@ -97,34 +71,30 @@ public extension Pcg64Random {
     }
 
     func backstep(by delta: UInt128) {
-        advance(by: -delta)
+        advance(by: 0 &- delta)
     }
 }
 
 
 public extension Pcg64Random {
 
-    convenience init(seed: UInt64, lock: PcgRandomLocking) {
-        self.init(seed: UInt128(seed), lock: lock)
+    convenience init(seed: Int) {
+        self.init(seed: UInt128(truncatingIfNeeded: seed))
     }
 
-    convenience init(seed: UInt64, stream: UInt64) {
-        self.init(seed: UInt128(seed), stream: UInt128(stream))
+    convenience init(seed: Int, stream: Int) {
+        self.init(seed: UInt128(truncatingIfNeeded: seed), stream: UInt128(truncatingIfNeeded: stream))
     }
 
-    convenience init(seed: UInt64) {
-        self.init(seed: UInt128(seed))
+    func advance(by delta: Int) {
+        advance(by: UInt128(truncatingIfNeeded: delta))
     }
 
-    func advance(by delta: UInt64) {
-        advance(by: UInt128(delta))
+    func discard(_ delta: Int) {
+        discard(UInt128(truncatingIfNeeded: delta))
     }
 
-    func discard(_ delta: UInt64) {
-        discard(UInt128(delta))
-    }
-
-    func backstep(by delta: UInt64) {
-        backstep(by: UInt128(delta))
+    func backstep(by delta: Int) {
+        backstep(by: UInt128(truncatingIfNeeded: delta))
     }
 }
